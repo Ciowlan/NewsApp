3d52a439b495f67eff86db1b91ba4bbb1e639d21
Student <Student@205-40>
2023-11-07 15:56:22 +0800

    加入專案檔案。

---
+++ NewsApp/Pages/NewsListPage.xaml.cs
@@ -0,0 +1,26 @@
+using NewsApp.Models;
+using NewsApp.Services;
+namespace NewsApp.Pages;
+
+public partial class NewsListPage : ContentPage
+{
+    public List<Article> ArticlesList;
+	public NewsListPage(string categoryName)
+	{
+		InitializeComponent();
+		Title = categoryName;
+		GetNews(categoryName);
+        ArticlesList = new List<Article>();
+	}
+
+	private async void GetNews(string categoryName)
+	{
+        var apiService = new ApiService();
+        var newsResult = await apiService.GetNews(categoryName);
+        foreach (var item in newsResult.Articles)
+        {
+            ArticlesList.Add(item);
+        }
+        CvNews.ItemsSource = ArticlesList;
+    }
+}
